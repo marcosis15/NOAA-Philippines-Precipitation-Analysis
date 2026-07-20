b7dfa7f4961f4a9c4335b9a2740fef0935402a92
marcosis15 <marcosison.1558@gmail.com>
2026-07-20 09:42:15 -0400

    Update noaa_data_readme.py

--- noaa_data_readme.py
+++ noaa_data_readme.py
@@ -1,4 +1,5 @@
-"""NOAA Data Readme"""
+# NOAA-Philippines-Precipitation-Analysis
+This project provides a comprehensive data analysis of precipitation data collected from various weather stations across the Philippines, spanning from the year 2000 to 2026. The primary objective is to uncover and understand key precipitation patterns, variations, and trends influenced by geographic region, month, and year.
 
 # Precipitation Pattern Analysis in the Philippines (2000-2026)
 
